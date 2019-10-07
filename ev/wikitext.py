bbce2b7af89098d39cff6bc70a757c3eafe3cf17
tomashege <41945595+tomashege@users.noreply.github.com>
2019-10-07 23:38:22 +0200

    removed the modula

--- ev/wikitext.py
+++ ev/wikitext.py
@@ -3,12 +3,13 @@ from gtts import gTTS
 import os
 print("Starting...")
 fileName = "text.txt"
-file = open(fileName,"r")
-print("file open")
-files = file.read()
-file.close()
-print("text file loaded")
+#file = open(fileName,"r")
+#print("file open")
+#files = file.read()
+#file.close()
+#print("text file loaded")
+#print("mp3 made")
+#print("Done")
+
 tts = gTTS(text=files, lang='en')
 tts.save("audio.mp3")
-print("mp3 made")
-print("Done")
